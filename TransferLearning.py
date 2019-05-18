b61410ca4bddc7f15c915924fc05c3cb78cf4b7a
VefBilge <vefugur@gmail.com>
2019-05-18 17:01:55 +0300

    Update TransferLearning.py

--- TransferLearning.py
+++ TransferLearning.py
@@ -5,6 +5,12 @@ Created on Tue May 14 19:44:47 2019
 @author: vefa
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 19:44:47 2019
+@author: vefa
+"""
+
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np 
 import pandas as pd
@@ -27,9 +33,11 @@ list_category2=[str(each) for each in data["Category2"].unique()]
 list_category3=[]
 list_category3=[str(each) for each in data["Category3"].unique()]
 #%%
-main_mean_array=np.load('ngram_array_x.npy')
-main_mean_array=np.reshape(main_mean_array,(len(main_mean_array),3000))
-
+main_mean_array=np.load('Main_Mean_Array.npy')
+vector_size=250
+main_mean_array=np.reshape(main_mean_array,(len(main_mean_array),vector_size))
+ngram_array=np.load('ngram_array_x.npy')
+ngram_array=np.reshape(ngram_array,(len(ngram_array),3000))
 #%%
 def RF(x,y):
     x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1)
@@ -38,10 +46,10 @@ def RF(x,y):
     y_pred = clf.predict(x_test)
     acc=metrics.accuracy_score(y_test, y_pred)
     print("Ürün içi başarı:{:.3f}".format(acc))
-    mat = confusion_matrix(y_test, y_pred)
-    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
-    plt.xlabel('true label')
-    plt.ylabel('predicted label')
+ #   mat = confusion_matrix(y_test, y_pred)
+ #   sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
+ #   plt.xlabel('true label')
+ #   plt.ylabel('predicted label')
     
 def RF_cross(x,y,x2,y2):
     clf=RandomForestClassifier(n_estimators=100)
@@ -49,10 +57,10 @@ def RF_cross(x,y,x2,y2):
     y2_pred=clf.predict(x2)
     acc=metrics.accuracy_score(y2, y2_pred)
     print("Ürünler Arası Başarı:{:.3f}".format(acc))
-    mat = confusion_matrix(y2, y2_pred)
-    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False   )
-    plt.xlabel('true label')
-    plt.ylabel('predicted label')
+#    mat = confusion_matrix(y2, y2_pred)
+#    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False   )
+ #   plt.xlabel('true label')
+ #   plt.ylabel('predicted label')
     
 def choiceProduct():
     urun1, urun2 = random.choices(list_category3, k=2)
@@ -61,6 +69,7 @@ def choiceProduct():
     return urun1, urun2, df1, df2
 
 def RFs(df1, df2):
+    print("\n\n Fasttext Model\n")  
     print('Urun 1 -> ' + df1.Category0.iloc[0] + '-' + df1.Category1.iloc[0] + '-' + df1.Category2.iloc[0] + '-' + df1.Category3.iloc[0])
     print('Urun 2 -> ' + df2.Category0.iloc[0] + '-' + df2.Category1.iloc[0] + '-' + df2.Category2.iloc[0] + '-' + df2.Category3.iloc[0])
     x1=[main_mean_array[each] for each in df1.index]
@@ -69,6 +78,22 @@ def RFs(df1, df2):
     print('Urun 1 ->', end=' ')
     RF(x1, y1)
     x2=[main_mean_array[each] for each in df2.index]
+    x2=np.array(x2)
+    y2=df2["Rate State"].values
+    print('Urun 2 ->', end=' ')
+    RF(x2, y2)
+    print('Urun 1 -> fit, Urun 2 -> predict', end=' ')
+    RF_cross(x1, y1, x2, y2)
+    print('Urun 1 -> predict, Urun 2 -> fit', end=' ')
+    RF_cross(x2, y2, x1, y1)
+    
+    print("\n\n 3-gram Model \n")  
+    x1=[ngram_array[each] for each in df1.index]
+    x1=np.array(x1)
+    y1=df1["Rate State"].values
+    print('Urun 1 ->', end=' ')
+    RF(x1, y1)
+    x2=[ngram_array[each] for each in df2.index]
     x2=np.array(x2)
     y2=df2["Rate State"].values
     print('Urun 2 ->', end=' ')
@@ -110,17 +135,4 @@ for each in range(5):
     print('')
     RFs(df1, df2)
 #%%
-df=data[data.Category3==list_category3[2]]
-x=[main_mean_array[each] for each in df.index]
-x=np.array(x)
-y=[data["Rate State"].values[each] for each in df.index]
-RF(x,y)
 
-#%%
-df=data[data.Category3==list_category3[3]]
-x2=[main_mean_array[each] for each in df.index]
-x2=np.array(x2)
-y2=[data["Rate State"].values[each] for each in df.index]
-RF(x2,y2)
-#%%
-RF_cross(x,y,x2,y2)
